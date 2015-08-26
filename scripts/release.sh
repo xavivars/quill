c07c301557b59103ec58257eed7283f7d5f83a05
Pedro Machado Santa <pedro.santa@gmail.com>
2015-08-26 10:14:22 +0100

    Add release script

---
+++ scripts/release.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+echo "Reedsy Quill Fork release."
+echo ""
+
+# Init
+VERSION=$1
+
+if [ "$VERSION" = "" ]
+then
+  echo "You must provide a release version!"
+  echo "Exiting"
+  exit
+else
+  echo "Is $VERSION the correct version?"
+  select yn in "Yes" "No"; do
+      case $yn in
+          Yes ) break;;
+          No ) exit;;
+      esac
+  done
+fi
+
+# Make sure the release is coming from the master branch
+CURRENT_BRANCH=$(git symbolic-ref -q HEAD)
+CURRENT_BRANCH=${CURRENT_BRANCH##refs/heads/}
+CURRENT_BRANCH=${CURRENT_BRANCH:-HEAD}
+
+if [ "$CURRENT_BRANCH" != "master" ]
+then
+  echo "You must be releasing from the master branch."
+  exit
+fi
+
+# Create temporary branch
+git checkout -b release-$VERSION
+
+# Dist it up
+grunt dist
+
+# Sort out the .gitignore
+echo "" >> ./.gitignore
+echo "!/dist" >> ./.gitignore
+
+# Add and commit the dist files
+git add ./dist
+git commit -m "Build $VERSION"
+
+# Tag it up
+git tag v$VERSION
+
+# Push it up
+git push origin refs/tags/v$VERSION
+
+# Revert all the changes
+git checkout master
+git checkout .gitignore
+git branch -D release-$VERSION
