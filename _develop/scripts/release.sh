b149132b3fd33fe76c9177d75aa114af329c7921
Xavi Ivars <xavi.ivars@gmail.com>
2023-03-15 00:22:08 +0100

    npm run prepare

--- _develop/scripts/release.sh
+++ _develop/scripts/release.sh
@@ -14,8 +14,7 @@ rm -r dist
 mkdir .release
 mkdir .release/quill
 
-npm run build
-npx webpack --config _develop/webpack.config.js --env minimize
+npm run prepare
 cp dist/quill.core.css dist/quill.bubble.css dist/quill.snow.css dist/quill.js dist/quill.core.js dist/quill.min.js dist/quill.min.js.map .release/quill/
 
 cd .release
